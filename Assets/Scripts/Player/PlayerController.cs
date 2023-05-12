1f8900b259620c2f1f5d38ff55aa6808939b070f
taihuynhz <guoba2023@gmail.com>
2023-05-13 01:06:05 +0700

    Update Lesson_4

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController: MonoBehaviour
 {   
     protected enum DriveMode { Manual, Automatic }
